61f88c123c2dce89d5180a35e9bb7bbe0a6edd9d
nickornator9000 <nicholasfulton14@gmail.com>
2024-03-23 13:20:31 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
 #coordinate tasks
+import processData
+import modelData
+from build.scripts.configSingleton import SingletonClass
+
+if __name__ == "__main__":
+    #do stuff
+    print("Hello world")
